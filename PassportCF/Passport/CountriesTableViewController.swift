dc84d34db9c4d0cc53a6a5080ca5d6b674799b94
ChristopherWFrost <ChristopherWFrost@gmail.com>
2015-11-19 20:15:05 -0800

    PassportCF

---
+++ PassportCF/Passport/CountriesTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CountriesTableViewController.swift
+//  Passport
+//
+//  Created by Christopher Frost on 11/19/15.
+//  Copyright (c) 2015 ChristopherWFrost. All rights reserved.
+//
+
+import UIKit
+
+class CountriesTableViewController: UITableViewController {
+    
+    var countries = ["Italy", "Norway", "England"]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        var country = countries[indexPath.row]
+        cell.textLabel!.text = country
+        return cell
+    }
+
+}
